46d6e39ca8b649104c35ebac8ca37bd0188d0797
Antonio Feitosa <arfneto@y7mail.com>
2017-06-02 04:42:57 +0000

    UI is a bit better. js still inline

--- public/code/circles.js
+++
@@ -1,9 +0,0 @@
-var canvas =null;
-window.onload =
-    function()
-    {
-        console.log("circle js" + canvas);
-        canvas = document.getElementById("myCanvas");
-        paper.setup(canvas);
-    };
-    
