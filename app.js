d038072b764c786dc626dcd50b41daef4358c07a
Antonio Feitosa <arfneto@y7mail.com>
2018-05-15 01:49:48 +0000

    just to deploy again

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require("express");
 var app = express();
 var bodyParser  = require("body-parser");
 
-var paper = require("paper");
 var thisTime = "circles";
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,22 +15,6 @@ app.get(
         res.render(thisTime);
     }
 );
-
-// app.post(
-//     "/",
-//     function(req, res)
-//     {
-//         var rows = req.body.nRows;
-//         var seconds = req.body.nSeconds;
-//         var animate = req.body.animate;
-//         var change = req.body.change;
-//         console.log("post route " + rows);
-//         console.log("animate " + animate);
-//         console.log("change " + change);
-//         console.log("seconds " + seconds);
-//         res.redirect("/");
-//     }  // end function()
-// );  // end app.post()
 
 app.listen(
     process.env.PORT, 
